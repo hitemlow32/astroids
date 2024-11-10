f7736fad3f8fc303a37da8642bf96c5520d6621e
hitemlow32 <hitemlow32.com>
2024-11-10 17:25:47 -0500

    Can print a black screen.

--- astroids/main.py
+++ astroids/main.py
@@ -5,9 +5,20 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
 
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 0 != 1 :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip() 
+
+    pygame.quit()
 main()
