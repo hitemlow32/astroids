52637611015047e8c3c1afc325b6a9b11edc0970
hitemlow32 <hitemlow32.com>
2024-11-12 13:00:11 -0500

    completed 4.3

--- astroids/main.py
+++ astroids/main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
@@ -13,12 +10,19 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+    FPS = 60
+
     while 0 != 1 :
+        ## allows the GUI to end the buttom if the x is clicked ##
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0,0,0))
         pygame.display.flip() 
+        dt = clock.tick(FPS) / 1000
 
     pygame.quit()
 main()
